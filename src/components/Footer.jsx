1cac6a831756518ad3995446a115a9371363b0a0
Elizabeth Czarny <liz.cz91@gmail.com>
2023-04-07 09:44:54 +0200

    fixed padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="w-full">
-      <ul className="flex gap-4 justify-center py-9">
+      <ul className="flex gap-4 justify-center p-9">
         <li>
           <Link to="/">Categorizer.io</Link>
         </li>
         <li>
-          <Link to="/">Terms & Privacy</Link>
+          <Link to="/">Terms &amp; Privacy</Link>
         </li>
         <li>
           <Link to="/">About</Link>
